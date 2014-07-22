98a8f88314efc83dd253d78cfd968ca2bf3bc865
Jeff Smith <jbs@oscium.com>
2014-07-22 16:12:39 -0500

    Fixed defrecord outside of module issue

--- lib/consumer.ex
+++ lib/consumer.ex
@@ -1,7 +1,10 @@
-defrecord Amqp.Consumer, amqp: nil do
-	def start(uri,fun,self) do
-		self = self.amqp(Amqp.Server.new.connect(uri))
-		self.amqp.receive(self.amqp.uri.exchange, self.amqp.uri.key, self.amqp.uri.queue)
-		self.amqp.wait fun
-	end
+defmodule Amqp.Consumer
+  import Record 
+  defrecord Amqp.Consumer, amqp: nil do
+    def start(uri,fun,self) do
+      self = self.amqp(Amqp.Server.new.connect(uri))
+      self.amqp.receive(self.amqp.uri.exchange, self.amqp.uri.key, self.amqp.uri.queue)
+      self.amqp.wait fun
+    end
+  end
 end
