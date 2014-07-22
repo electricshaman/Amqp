9b854d5a6839796b34c49113d55bff78a03de75d
Jeff Smith <jbs@oscium.com>
2014-07-22 16:21:00 -0500

    Fixed another defmodule issue

--- lib/producer.ex
+++ lib/producer.ex
@@ -1,8 +1,10 @@
-defrecord Amqp.Producer, amqp: nil do
-	def start(uri,self) do
-		self.amqp(Amqp.Server.new.connect(uri))
-	end
-	def send(message,self) do
-		self.amqp.send self.amqp.uri.exchange, self.amqp.uri.key, message
-	end
+defmodule Amq.Producer do
+  defrecord Amqp.Producer, amqp: nil do
+    def start(uri,self) do
+      self.amqp(Amqp.Server.new.connect(uri))
+    end
+    def send(message,self) do
+      self.amqp.send self.amqp.uri.exchange, self.amqp.uri.key, message
+    end
+  end
 end
